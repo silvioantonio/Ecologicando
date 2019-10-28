53d57070db3eeafa091e0b903f2fb9e5852117fd
Silvio Antonio <oliveira_0000@hotmail.com>
2019-10-28 11:07:51 -0300

    Criando menu superior com fragment

--- app/src/main/java/com/silvio/ecologicando/MainActivity.java
+++ app/src/main/java/com/silvio/ecologicando/MainActivity.java
@@ -2,9 +2,18 @@ package com.silvio.ecologicando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements ToolbarFragment {
+public class MainActivity extends Activity {
 
 
     @Override
@@ -12,10 +21,37 @@ public class MainActivity extends AppCompatActivity implements ToolbarFragment {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
 
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.share:
+                Toast.makeText(getApplicationContext(),"share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about:
+                Toast.makeText(getApplicationContext(), "about", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit:
+                Toast.makeText(getApplicationContext(), "exit", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search:
+                Toast.makeText(getApplicationContext(), "search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting:
+                Toast.makeText(getApplicationContext(), "setting", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/silvio/ecologicando/ToolbarFragment.java
+++ app/src/main/java/com/silvio/ecologicando/ToolbarFragment.java
@@ -2,47 +2,36 @@ package com.silvio.ecologicando;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 
 public class ToolbarFragment extends Fragment {
 
+    public ToolbarFragment(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.toolbar_fragment_menu, container,false);
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu,menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
-        switch (id){
-            case R.id.share:
-                Toast.makeText(getContext().getApplicationContext(),"share", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.about:
-                Toast.makeText(getContext().getApplicationContext(), "about", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.exit:
-                Toast.makeText(getContext().getApplicationContext(), "exit", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.search:
-                Toast.makeText(getContext().getApplicationContext(), "search", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.setting:
-                Toast.makeText(getContext().getApplicationContext(), "setting", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        return true;
-    }
+
 
 }
