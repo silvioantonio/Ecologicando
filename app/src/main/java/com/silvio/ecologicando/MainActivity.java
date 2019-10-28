af2e111523983fc955c180fec3ed0866784997bb
Silvio Antonio <oliveira_0000@hotmail.com>
2019-10-28 11:38:39 -0300

    Ajustando menu superior

--- app/src/main/java/com/silvio/ecologicando/MainActivity.java
+++ app/src/main/java/com/silvio/ecologicando/MainActivity.java
@@ -3,14 +3,10 @@ package com.silvio.ecologicando;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -20,14 +16,12 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+        new MenuInflater(this).inflate(R.menu.menu, menu);
+        return (super.onCreateOptionsMenu(menu));
     }
 
     @Override
