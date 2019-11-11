1a4d59ff67b561aa10cea65efdc2d43ea9ffac8c
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-11 09:29:10 -0300

    Retirado barra de menu

--- app/src/main/java/com/silvio/ecologicando/MainActivity.java
+++ app/src/main/java/com/silvio/ecologicando/MainActivity.java
@@ -2,10 +2,6 @@ package com.silvio.ecologicando;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,37 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        switch (id){
-            case R.id.share:
-                Toast.makeText(getApplicationContext(),"share", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.about:
-                Toast.makeText(getApplicationContext(), "about", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.exit:
-                Toast.makeText(getApplicationContext(), "exit", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.search:
-                Toast.makeText(getApplicationContext(), "search", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.setting:
-                Toast.makeText(getApplicationContext(), "setting", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        return true;
     }
 
 }
