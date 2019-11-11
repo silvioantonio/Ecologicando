c98068bff1a38bcd09ba3321cd7fe4b4967f4b26
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-11 15:41:30 -0300

    Criação de activity para 2 tela e criaçao de logo para navegaçao

---
+++ app/src/main/java/com/silvio/ecologicando/CriandoSeuAdubo.java
@@ -0,0 +1,14 @@
+package com.silvio.ecologicando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriandoSeuAdubo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criando_seu_adubo);
+    }
+}
