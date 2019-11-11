7e6d18f5331c7e59c8641b7a7626a35fcb99ab19
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-11 09:53:42 -0300

    Criado cores primarias do app

--- app/src/main/java/com/silvio/ecologicando/ToolbarFragment.java
+++ app/src/main/java/com/silvio/ecologicando/ToolbarFragment.java
@@ -22,7 +22,6 @@ public class ToolbarFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
     }
 
 }
