b13e42cec0168b2e23982bd04176ac8aa5dc6d72
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-12 11:08:57 -0300

    Fragment layout para banana e criacao de classe fragment

--- app/src/main/java/com/silvio/ecologicando/CriandoSeuAdubo.java
+++ app/src/main/java/com/silvio/ecologicando/CriandoSeuAdubo.java
@@ -1,12 +1,14 @@
 package com.silvio.ecologicando;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class CriandoSeuAdubo extends AppCompatActivity {
+public class CriandoSeuAdubo extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private BottomNavigationView navigationView;
 
@@ -16,5 +18,19 @@ public class CriandoSeuAdubo extends AppCompatActivity {
         setContentView(R.layout.activity_criando_seu_adubo);
 
         navigationView = findViewById(R.id.navigationView);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.navegacao_banana:
+                break;
+            case R.id.navegacao_ovos:
+                break;
+            case R.id.navegacao_cafe:
+                break;
+        }
+        return true;
     }
 }
