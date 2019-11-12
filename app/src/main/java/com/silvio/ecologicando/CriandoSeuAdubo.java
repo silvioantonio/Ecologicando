91ec20dff9df43317d0c95f279ed6d6025d189ee
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-12 10:45:30 -0300

    Ajustando layout de navegaçao

--- app/src/main/java/com/silvio/ecologicando/CriandoSeuAdubo.java
+++ app/src/main/java/com/silvio/ecologicando/CriandoSeuAdubo.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class CriandoSeuAdubo extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criando_seu_adubo);
+
+        navigationView = findViewById(R.id.navigationView);
     }
 }
