ba7163f5b9b596fdea893bc921a5960bf9ed4631
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-21 21:57:12 -0300

    Criaçao e ajuste de bottom menu

---
+++ app/src/main/java/com/silvio/ecologicando/model/GarrafasOrganizador.java
@@ -0,0 +1,21 @@
+package com.silvio.ecologicando.model;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.silvio.ecologicando.R;
+
+public class GarrafasOrganizador extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_organizador, container, false);
+    }
+
+    public static GarrafasOrganizador newInstance() {
+        return new GarrafasOrganizador();
+    }
+}
