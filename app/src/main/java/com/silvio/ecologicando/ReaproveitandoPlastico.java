b328cdb77ae5a484da83a322ff154f20755292f9
Silvio Antonio <oliveira_0000@hotmail.com>
2019-11-21 21:29:06 -0300

    criaçao de activity

---
+++ app/src/main/java/com/silvio/ecologicando/ReaproveitandoPlastico.java
@@ -0,0 +1,14 @@
+package com.silvio.ecologicando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReaproveitandoPlastico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reaproveitando_plastico);
+    }
+}
---
+++ app/src/main/java/com/silvio/ecologicando/SeparandoLixo.java
@@ -0,0 +1,14 @@
+package com.silvio.ecologicando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeparandoLixo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_separando_lixo);
+    }
+}
