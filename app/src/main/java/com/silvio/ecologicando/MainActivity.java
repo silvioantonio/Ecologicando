fd75fec6b939fe821d2954012fa77f42068e877d
Silvio Antonio <oliveira_0000@hotmail.com>
2019-12-03 21:27:07 -0300

    Configuraçao de mensagem dinamica para usuario

--- app/src/main/java/com/silvio/ecologicando/MainActivity.java
+++ app/src/main/java/com/silvio/ecologicando/MainActivity.java
@@ -4,22 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable{
 
     private ImageView btn1, btn2, btn3, btn4;
+    private TextView textView;
+    private String[] frases;
+    private int cont=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.frase);
+
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
         btn4 = findViewById(R.id.btn4);
+
+        frases = getResources().getStringArray(R.array.array);
+
+        Handler handler = new Handler();
+        handler.postDelayed( this, 1500);
+
+
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +60,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SeparandoLixo.class));
             }
         });
+    }
+
+    @Override
+    public void run(){
+        if(cont<2)
+            textView.setText(frases[cont]);
+        else {
+            cont = 0;
+            textView.setText(frases[cont]);
+        }
+        cont++;
     }
 
 }
